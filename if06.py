6aa6cfa52f4ada3aeba2cd4792bb92d125a228f9
Muxtor <jalilovmuxtor203@gmail.com>
2022-11-21 23:07:18 +0500

    06

--- if06.py
+++ if06.py
@@ -13,4 +13,13 @@ def main(a,b,c):
     Returns:
         string: string with the result
     """
-    return
+    if a<0 and b<0 and c<0:
+        return 'manfiy sonlar ko`p'
+    if a<0 and b<0 and c>0 or a>0 and b<0 and c<0 or a<0 and b>0 and c<0:
+        return 'manfiy sonlar ko`p'
+    if a<0 and b>0 and c>0 or a>0 and b>0 and c<0 or a>0 and b<0 and c>0:
+        return 'musbat sonlar ko`p'
+    else:
+        return 0
+print(main(1,3,-6))
+    
