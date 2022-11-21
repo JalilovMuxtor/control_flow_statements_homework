aa5deaddd40188f51f842e91236892512a28c1ee
Muxtor <jalilovmuxtor203@gmail.com>
2022-11-21 22:20:13 +0500

    04

--- if04.py
+++ if04.py
@@ -8,4 +8,12 @@ def main(a,b,c):
     returns:
         integer: the number of positive numbers in the given numbers
     """
-    return
+    if a>0 and b>0 and c>0:
+        return "3 ta musbat sonla "
+    if a>0 and b>0 and c<0 or b>0 and a<0 and c>0 or b<0 and a>0 and c>0:
+        return "2ta musbat 1ta manfiy"
+    if a<0 and b<0 and c>0 or a>0 and b<0 and c<0 or b>0 and a<0 and c<0:
+        return "2 ta manfiy 1ta musbat"
+    else:
+        return " 3 ta manfiy son"
+print(main(-3,-9,5))
