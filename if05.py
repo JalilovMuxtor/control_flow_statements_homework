6c5fa4d640ba3b020f388ad659ee3495d9a2e7a2
Muxtor <jalilovmuxtor203@gmail.com>
2022-11-21 22:56:20 +0500

    05

--- if05.py
+++ if05.py
@@ -13,6 +13,7 @@ def main(a,b,c):
     if a<0 and b<0 and c>0 or a>0 and b<0 and c<0 or a<0 and b>0 and c<0:
         return 2
     if a<0 and b>0 and c>0 or a>0 and b>0 and c<0 or a>0 and b<0 and c>0:
-        return 0
-    return 1
+        return 1
+    else:
+        return 1
 print(main(2,-5,-6))
